7b7d88e74db1599ec88b84e6493ae5e4a3513652
mark-scholes <innominatus15@outlook.com>
2020-05-06 08:57:10 +0100

    Added the images that will be used for the game. Also added some basic styling to the page.

---
+++ app.js
@@ -0,0 +1,24 @@
+const cards = [
+  { name: "Ripley", image: "images/5e5dcfa5d4d9e61312bec3a021072685.jpeg" },
+  { name: "Ripley", image: "images/5e5dcfa5d4d9e61312bec3a021072685.jpeg" },
+  {
+    name: "Alien_isolation",
+    image: "4229e095-401e-45d9-8e9f-9fcc939e86eb.jpg",
+  },
+  {
+    name: "Alien_isolation",
+    image: "4229e095-401e-45d9-8e9f-9fcc939e86eb.jpg",
+  },
+  { name: "giger_xenomorph", image: "alien.jpg" },
+  { name: "giger_xenomorph", image: "alien.jpg" },
+  { name: "alien_covernant", image: "covenant.jpg" },
+  { name: "alien_covernant", image: "covenant.jpg" },
+  { name: "egg", image: "D4NlGIR9TQmfm1kTUK0j-PFM7ktP2KpgKt2EG-xqKTo.png" },
+  { name: "egg", image: "D4NlGIR9TQmfm1kTUK0j-PFM7ktP2KpgKt2EG-xqKTo.png" },
+  { name: "xenomorph", image: "IM8hOIC.jpg" },
+  { name: "xenomorph", image: "IM8hOIC.jpg" },
+  { name: "bullet_baby", image: "images.jpg" },
+  { name: "bullet_baby", image: "images.jpg" },
+  { name: "xenomorph_face", image: "vfAFwnTF_400x400.jpg" },
+  { name: "xenomorph_face", image: "vfAFwnTF_400x400.jpg" },
+];
