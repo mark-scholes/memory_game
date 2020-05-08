5a4efed951dba98e402fb023f93c7c977416a61c
mark-scholes <innominatus15@outlook.com>
2020-05-08 03:41:22 +0100

    Most of the game is actually working now. Some images needed to be replaced because they were not the same size. Some changes were made to the logic to allow the correct image to be shown when the user clicked on the card.

--- app.js
+++ app.js
@@ -9,27 +9,34 @@ const cards = [
     name: "Alien_isolation",
     image: "4229e095-401e-45d9-8e9f-9fcc939e86eb.jpg",
   },
-  { name: "giger_xenomorph", image: "alien.jpg" },
-  { name: "giger_xenomorph", image: "alien.jpg" },
-  { name: "alien_covernant", image: "covenant.jpg" },
-  { name: "alien_covernant", image: "covenant.jpg" },
-  { name: "egg", image: "D4NlGIR9TQmfm1kTUK0j-PFM7ktP2KpgKt2EG-xqKTo.png" },
-  { name: "egg", image: "D4NlGIR9TQmfm1kTUK0j-PFM7ktP2KpgKt2EG-xqKTo.png" },
-  { name: "xenomorph", image: "IM8hOIC.jpg" },
-  { name: "xenomorph", image: "IM8hOIC.jpg" },
-  { name: "bullet_baby", image: "images.jpg" },
-  { name: "bullet_baby", image: "images.jpg" },
-  { name: "xenomorph_face", image: "vfAFwnTF_400x400.jpg" },
-  { name: "xenomorph_face", image: "vfAFwnTF_400x400.jpg" },
+  { name: "giger_xenomorph", image: "images/alien.jpg" },
+  { name: "giger_xenomorph", image: "images/alien.jpg" },
+  { name: "alien_covernant", image: "images/covenant.jpg" },
+  { name: "alien_covernant", image: "images/covenant.jpg" },
+  {
+    name: "egg",
+    image: "images/D4NlGIR9TQmfm1kTUK0j-PFM7ktP2KpgKt2EG-xqKTo.png",
+  },
+  {
+    name: "egg",
+    image: "images/D4NlGIR9TQmfm1kTUK0j-PFM7ktP2KpgKt2EG-xqKTo.png",
+  },
+  { name: "xenomorph", image: "images/IM8hOIC.jpg" },
+  { name: "xenomorph", image: "images/IM8hOIC.jpg" },
+  { name: "bullet_baby", image: "images/images.jpg" },
+  { name: "bullet_baby", image: "images/images.jpg" },
+  { name: "xenomorph_face", image: "images/vfAFwnTF_400x400.jpg" },
+  { name: "xenomorph_face", image: "images/vfAFwnTF_400x400.jpg" },
 ];
 
 // setting up the game board
 
 cards.sort(() => 0.5 - Math.random());
 
-const grid = document.querySelector(".grid");
-const resultsDisplay = document.querySelector("#result");
-let cardChosen = [];
+const grid = document.getElementById("grid");
+console.log(document.getElementById("grid"));
+const resultsDisplay = document.getElementById("result");
+let cardsChosen = [];
 let cardsChosenId = [];
 let cardsFound = [];
 
@@ -37,7 +44,7 @@ createBoard = () => {
   for (let i = 0; i < cards.length; i++) {
     let card = document.createElement("img");
     card.setAttribute("src", "images/blank.png");
-    card.setAttribute("data-id", i);
+    card.setAttribute("id", i);
     card.addEventListener("click", flipCard);
     grid.appendChild(card);
   }
@@ -47,6 +54,7 @@ checkForMatch = () => {
   let allCards = document.querySelectorAll("img");
   const optionOneId = cardsChosenId[0];
   const optionTwoId = cardsChosenId[1];
+  console.log(cardsChosenId);
   if (cardsChosen[0] === cardsChosen[1]) {
     alert("You found a match");
     cards[optionOneId].setAttribute("src", "images/white.png");
@@ -60,16 +68,21 @@ checkForMatch = () => {
   cardsChosenId = [];
 
   cardsChosen = [];
-  resultsDisplay;
+  resultsDisplay.textContent = cardsFound.length;
+  if (cardsFound.length === cards.length / 2) {
+    resultsDisplay.textContent = "Well done, you found all the matches";
+  }
 };
 
-flipCard = () => {
-  let cardId = this.getAttribute("data-id");
-  cardChosen.push(cards[cardId.name]);
+flipCard = (e) => {
+  let cardId = e.target.id;
+
+  cardsChosen.push(cards[cardId]);
+
   cardsChosenId.push(cardId);
 
-  this.setAttribute("src", cards[cardId].img);
-  if (cardChosen.length === 2) {
+  e.target.setAttribute("src", cards[cardId].image);
+  if (cardsChosen.length === 2) {
     setTimeout(checkForMatch, 500);
   }
 };
